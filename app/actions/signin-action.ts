0f67c7a49807a7b7b7105abff2f687922e381c43
BoomTHDev <minecraft8231@gmail.com>
2024-08-30 14:06:07 +0700

    fix signin action

--- app/actions/signin-action.ts
+++ app/actions/signin-action.ts
@@ -1,54 +1,55 @@
 "use server";
 
-import { prisma } from "@/lib/prisma";
-import { cookies } from "next/headers";
+import { PrismaClient } from "@prisma/client";
 import * as jose from "jose";
 import bcrypt from "bcrypt";
+import { cookies } from 'next/headers'
+
+const prisma = new PrismaClient();
 
 export async function signin(formData: FormData) {
   const username = formData.get("username") as string;
   const password = formData.get("password") as string;
 
-  try {
-    if (username && password) {
-      const user = await prisma.user.findFirst({
-        where: {
-          username,
-        },
-      });
+  // find user
+  if (username && password) {
+    const user = await prisma.user.findFirst({
+      where: {
+        username,
+      },
+    });
 
-      if (!user) {
-        return { error: "ไม่พบผู้ใช้", success: false };
-      }
+    // check user
+    if (!user) {
+      return { message: "ไม่พบผู้ใช้", type: 'error' };
+    }
 
-      const isValid = await bcrypt.compare(password, user.password);
-      if (!isValid) {
-        return { error: "รหัสผ่านไม่ถูกต้อง", success: false };
-      }
+    // compare password
+    const correctPass = await bcrypt.compare(password, user.password);
+    if (!correctPass) {
+      return { message: "รหัสผ่านไม่ถูกต้อง", type: 'error' };
+    }
 
-      // create jwt token
-      const secret = new TextEncoder().encode(
-        process.env.NEXT_PUBLIC_SECRET_TOKEN
-      );
-      const alg = "HS256";
-      const jwt = await new jose.SignJWT({
-        username: user.username,
-      })
-        .setProtectedHeader({ alg })
-        .setExpirationTime("350h")
-        .setSubject(user.id.toString())
-        .sign(secret);
+    // create jwt token
+    const secret = new TextEncoder().encode(process.env.NEXT_PUBLIC_SECRET_TOKEN);
+    const alg = "HS256";
+    const jwt = await new jose.SignJWT({
+      username: user.username,
+    })
+      .setProtectedHeader({ alg })
+      .setExpirationTime("350h")
+      .setSubject(user.id.toString())
+      .sign(secret);
+
       // set cookies
       cookies().set("token_user", jwt, {
         secure: true,
         httpOnly: true,
         expires: Date.now() + 24 * 60 * 60 * 1000 * 13,
-        path: "/",
-        sameSite: "strict",
-      });
-      return { success: true, message: "เข้าสู่ระบบสำเร็จ" };
-    }
-  } catch (error: any) {
-    return { error: error.message, success: false };
+        path: '/',
+        sameSite: 'strict'
+    })
+
+    return { message: 'เข้าสู่ระบบสำเร็จ', type: 'success' }
   }
 }
